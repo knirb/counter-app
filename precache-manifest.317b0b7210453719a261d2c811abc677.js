d30730aac66c759a9a8e943a627d825cb29a11a5
Viktor Åsbrink <viktorasbrink@gmail.com>
2020-08-16 15:44:07 +0200

    Updates

---
+++ precache-manifest.317b0b7210453719a261d2c811abc677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b34dacc0b693d4bf43c5c7b1a1bd005",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "fa1bdba0da88c504c5e5",
+    "url": "/counter-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "4b07b025cba6e7feb9f4",
+    "url": "/counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa1bdba0da88c504c5e5",
+    "url": "/counter-app/static/js/2.f1f4cfba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.f1f4cfba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b07b025cba6e7feb9f4",
+    "url": "/counter-app/static/js/main.692afdd9.chunk.js"
+  },
+  {
+    "revision": "8d3c91def09e322d68af",
+    "url": "/counter-app/static/js/runtime-main.7c4e8537.js"
+  }
+]);
